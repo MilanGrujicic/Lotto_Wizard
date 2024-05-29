f76082f214d789a8a2d901d4af0d95399e5c15c4
Milan <milangru@gmail.com>
2024-05-29 14:09:10 +0200

    Added import of winning numbers to python script.

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ import subprocess
 
 file_to_run = f"./Loto_Check.robot"
 
-subprocess.run(["robot", file_to_run])
+subprocess.run(["robot", file_to_run], capture_output=True, text=True)
+
+with open('winning_numbers.txt', 'r') as f:
+    winning_numbers = f.read()
+ 
+played_numbers = ['5','11','13','21','38','4','10']
+print('Winning numbers:', winning_numbers)
