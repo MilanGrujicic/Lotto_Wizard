a3c00b0837a7c307f5513a6748348331accc04b7
Milan <milangru@gmail.com>
2024-05-30 02:33:48 +0200

    Added played numbers.

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ file_to_run = f"./Loto_Check.robot"
 
 subprocess.run(["robot", file_to_run], capture_output=True, text=True)
 
-with open('winning_numbers.txt', 'r') as f:
+with open("winning_numbers.txt", "r") as f:
     winning_numbers = f.read()
- 
-played_numbers = ['5','11','13','21','38','4','10']
-print('Winning numbers:', winning_numbers)
+
+played_numbers = ["5", "11", "13", "21", "38", "4", "10"]
+print("Winning numbers: ", winning_numbers, type(winning_numbers))
+print("Played numbers: ", played_numbers, type(played_numbers))
